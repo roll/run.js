2696f7a0d91eb9b809b2431611d308b31183e0e8
roll <eskarev@gmail.com>
2017-12-07 13:07:18 +0300

    Fixed faketty

--- src/faketty.js
+++ src/faketty.js
@@ -1,6 +1,8 @@
 // Module API
 
 function applyFaketty(code, {faketty}) {
+  // eslint-disable-next-line
+  code = `'${code.replace(/'/g, `'\\''`)}'`
   return faketty ? `script -qefc ${code}` : code
 }
 
